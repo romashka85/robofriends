a528301ab1718758b36da2d1ac4e90cd78cf01f7
Yulia <duratsia@students.softuni.bg>
2019-04-11 12:47:50 +0300

    add scroll component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Card.css';
 
 const Card = (props) => {
     const {id, name, email} = props;
     
     return (
-        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+        <div className='width tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
             <img alt='#' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{name}</h2>
