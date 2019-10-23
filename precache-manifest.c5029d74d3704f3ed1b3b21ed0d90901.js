8c49c0a969ccb8ddfe46135b2d5b86f40780b902
Yulia <duratsia@students.softuni.bg>
2019-10-23 10:49:33 +0300

    Updates

---
+++ precache-manifest.c5029d74d3704f3ed1b3b21ed0d90901.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d37d76ac171f6ff067d1ed55ffa30a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0903c11ed1f299d3ab1b",
+    "url": "/robofriends/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "47c37b6513d6d68e9488",
+    "url": "/robofriends/static/css/main.18267ca3.chunk.css"
+  },
+  {
+    "revision": "0903c11ed1f299d3ab1b",
+    "url": "/robofriends/static/js/2.61b6dae8.chunk.js"
+  },
+  {
+    "revision": "47c37b6513d6d68e9488",
+    "url": "/robofriends/static/js/main.cac9b763.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/robofriends/static/media/MR ROBOT.86c545cc.woff"
+  }
+]);
